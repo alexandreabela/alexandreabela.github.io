ede9a9c01f6bac755d8393686fad1e033b5001da
alexandreabela <alexandre.abela16@gmail.com>
2026-02-24 16:01:24 +0000

    Deploying to gh-pages from @ alexandreabela/alexandreabela.github.io@6effaae3e1ca89e6de5efbccdd77086d4092580f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,21 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-mastering-graph-ml-traditional-approaches-to-generating-graphs",
+        },{id: "post-mastering-graph-ml-traditional-approaches-to-generating-graphs",
         
           title: 'Mastering Graph ML: Traditional Approaches to Generating Graphs <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
